ef45e38009ec11532b05cee734144151dce79044
kimcore <kimcoreovo@gmail.com>
2022-11-29 16:39:09 +0900

    Remove unused deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     `maven-publish`
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.serialization") version "1.7.21"
-    id("com.github.otkmnb2783.dotenv") version "0.2.0"
 }
 
 group = "com.github.kimcore"
@@ -21,13 +20,7 @@ dependencies {
     implementation("io.ktor", "ktor-client-content-negotiation", "2.1.3")
     implementation("io.ktor", "ktor-serialization", "2.1.3")
     implementation("io.ktor", "ktor-serialization-kotlinx-json", "2.1.3")
-
-    implementation("org.jsoup", "jsoup", "1.15.3")
-
     implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.4.1")
-    implementation("org.slf4j", "slf4j-api", "2.0.5")
-    implementation("ch.qos.logback", "logback-classic", "1.4.5")
-    implementation("io.github.microutils", "kotlin-logging-jvm", "3.0.4")
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
