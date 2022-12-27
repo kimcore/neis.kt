128c8e198b48e1214c5c2304401298948bbe4168
kimcore <kimcoreovo@gmail.com>
2022-12-27 16:01:03 +0900

    fix startOfWeek and endOfWeek not returning proper dates

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.kimcore"
-version = "1.3"
+version = "1.4"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/github/kimcore/neis/utils.kt
+++ src/main/kotlin/com/github/kimcore/neis/utils.kt
@@ -4,12 +4,13 @@ import java.time.LocalDate
 import java.time.ZoneId
 import java.util.*
 
-private val calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"))
+private val calendar: Calendar
+    get() = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"))
 private val zone = ZoneId.of("Asia/Seoul")
-fun startOfWeek(): LocalDate = LocalDate.ofInstant((calendar.clone() as Calendar).apply {
+fun startOfWeek(): LocalDate = LocalDate.ofInstant(calendar.apply {
     set(Calendar.DAY_OF_WEEK, calendar.firstDayOfWeek)
 }.time.toInstant(), zone)
 
-fun endOfWeek(): LocalDate = LocalDate.ofInstant((calendar.clone() as Calendar).apply {
+fun endOfWeek(): LocalDate = LocalDate.ofInstant(calendar.apply {
     set(Calendar.DAY_OF_WEEK, calendar.firstDayOfWeek + 6)
 }.time.toInstant(), zone)
